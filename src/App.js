dd60364e942fdb39444ce57b96d86b36c4b4af23
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2020-09-09 12:09:04 +0700

    Create SeasonDisplay

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import './App.css';
 import 'semantic-ui-css/semantic.min.css'
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends Component {
   state = { lat: null, errorMessage: '' };
@@ -17,7 +17,7 @@ class App extends Component {
       return <div> Error : { this.state.errorMessage } </div>
     }
     if (!this.state.errorMessage && this.state.lat) {
-      return <div> Latitude : { this.state.lat } </div>
+      return <SeasonDisplay lat={ this.state.lat }/>
     }
     return <div> Loading! </div>
   }
---
+++ src/SeasonDisplay.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SeasonDisplay() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
